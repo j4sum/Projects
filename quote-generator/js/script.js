c0967d8558c6f07fe25af0ea4c7151cc157bdf01
Sumeyya Javaid <jasum@protonmail.com>
2022-08-30 15:15:46 +0100

    add active class dynamically

---
+++ quote-generator/js/script.js
@@ -0,0 +1,8 @@
+// function makeActive() {
+//     document.getElementsByClassName('lds-ring').classList.add('active');
+// }
+
+function makeActive() {
+    let element = document.getElementsByClassName('lds-ring-container');
+    element.classList.add("active");
+}
